d154516268e9132c7232c2dd208aa1d97c15f3f8
Veronika1f <117941222+Veronika1f@users.noreply.github.com>
2022-12-13 15:09:59 +0200

    Add files via upload

---
+++ задача.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int mainL() 
+{
+    const int N = 10;
+    int array[N];
+    int sum = 0;
+    int el = 10000;
+    for (int j = 0; j <N; j++){
+        cout <<"array["<< j <<"]=";
+        cin >> array[j];   //введення чисел масиву
+    }
+    for (int j = 0; j < 10 ;j++)
+           cout <<array[j]<<"";//виведення чисел усього масиву
+    for(int j = 0; j < N; j++)       
+}     if (el > array[j])
+            el = array[j];//знаходження мінімального елемента масиву 
+
+
+      cout <<"/n"<<"min" <<endl;//виведення мінімального елемента масиву
+
+    for (int j =0; j<10; j++)  {
+        if(el<0)
+        {
+            int i=0;
+            int sum =0;
+            while(array[i++]>0)//пропускаємо всі невід'ємні і перший від'ємний
+            while(array[i]>=0)
+            sum+array[i++]
+        }
+        system("pause");
+        return 0;
+    }     
+    
